07f87dbadcceac468c8a0308f16697213b9c708d
Stanly Sukmajaya <v.stanly.sukmajaya@adira.co.id>
2021-09-02 19:30:17 +0700

    heroku setup

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,12 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
+
+const devConfig = `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.PORT}/${process.env.DATABASE}`;
+
+const proConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-  host: process.env.DATABASE_HOST,
-  port: 5432
+  connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig,
 });
 
 module.exports = pool;
