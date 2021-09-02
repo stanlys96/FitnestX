cb66731f82ce34c6b7bbc9f8fba5e7c3258deed6
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2021-09-02 20:29:00 +0700

    fix sql

--- server/database.sql
+++ server/database.sql
@@ -5,5 +5,7 @@ CREATE DATABASE fitnestx_database;
 CREATE TABLE users(
   user_id SERIAL PRIMARY KEY,
   first_name VARCHAR(255),
-  last_name VARCHAR(255)
+  last_name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
 );
