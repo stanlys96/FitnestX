77b1f385205f9fa6a172a805d3dcde39dfdaba2a
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2021-09-02 19:12:32 +0700

    heroku setup

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: "fitnestx_database",
+  database: process.env.DATABASE,
   host: process.env.DATABASE_HOST,
   port: 5432
 });
