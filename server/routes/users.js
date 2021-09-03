647871049fc8b243a74eeeb4ff791ddef4298a98
Stanly Sukmajaya <v.stanly.sukmajaya@adira.co.id>
2021-09-03 16:41:50 +0700

    setup completeProfile route

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 
 router.post('/register', UserController.register);
-router.post('/completeProfile', UserController.completeProfile);
+router.post('/completeProfile/:email', UserController.completeProfile);
 router.get('/getAll', UserController.getAllUsers);
 
 module.exports = router;
